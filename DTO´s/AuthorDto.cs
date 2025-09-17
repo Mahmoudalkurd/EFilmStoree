e66c0e2242b95b32cb1f32e9589231358eb87c41
mahmoud alkurd <mahmoudalkurdgbg@gmail.com>
2025-09-17 13:23:44 +0200

    Delete DTO´s directory

--- DTO´s/AuthorDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EBookStore.DTOs
-{
-    public class AuthorDto
-    {
-        public int AuthorId { get; set; }
-        public string Name { get; set; }
-    }
-}
--- DTO´s/BookDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EBookStore.DTOs
-{
-    public class BookDto
-    {
-        public string Title { get; set; }
-        public decimal Price { get; set; }
-        public int AuthorId { get; set; }  
-    }
-}
